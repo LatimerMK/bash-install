54a986f2ee94d5a25a2d255a15bca9868ce2ef3b
LatimerMK <maxlatimer16@gmail.com>
2024-10-13 15:01:40 +0300

    install Create desktop icon

--- Desktop_file/create_desktop_file.sh
+++ Desktop_file/create_desktop_file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # bash <(curl -s https://raw.githubusercontent.com/LatimerMK/bash-install/refs/heads/main/Desktop_file/create_desktop_file.sh) "My Application" "/path/to/your/executable" "/path/to/your/icon.png"
+
 # Перевірте, чи надано необхідні аргументи
 if [ "$#" -ne 3 ]; then
     echo "Використання: $0 <name> <exec_path> <icon_path>"
@@ -12,7 +13,7 @@ EXEC_PATH=$2
 ICON_PATH=$3
 
 # Створіть .desktop файл
-DESKTOP_FILE="$HOME/.local/share/applications/${NAME}.desktop"
+DESKTOP_FILE="$HOME/.local/share/applications/${NAME// /_}.desktop"  # Заміна пробілів на підкреслення
 
 echo "[Desktop Entry]" > "$DESKTOP_FILE"
 echo "Version=1.0" >> "$DESKTOP_FILE"
@@ -27,3 +28,12 @@ echo "Categories=Utility;" >> "$DESKTOP_FILE"
 chmod +x "$DESKTOP_FILE"
 
 echo "Файл $DESKTOP_FILE створено успішно!"
+
+# Додаємо додаток до Dock
+CURRENT_FAVORITES=$(gsettings get org.gnome.shell favorite-apps)
+
+# Оновлення списку улюблених застосунків
+NEW_FAVORITES=$(echo $CURRENT_FAVORITES | sed "s/]\$/,'${NAME// /_}.desktop']/")
+gsettings set org.gnome.shell favorite-apps "$NEW_FAVORITES"
+
+echo "Додаток ${NAME} додано до Dock."
