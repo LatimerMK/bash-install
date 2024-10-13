9e05c421f527148b209babfcb6d0031c72f2df4a
LatimerMK <maxlatimer16@gmail.com>
2024-10-13 20:11:34 +0300

    install gnome rdp add new user

--- GNOME_rdp/setup_gnome_xrdp.sh
+++ GNOME_rdp/setup_gnome_xrdp.sh
@@ -17,9 +17,9 @@ echo "Встановлюємо xfce4..."
 sudo apt install xfce4 -y
 
 # Додаємо користувача в групу ssl-cert
-echo "Додаємо користувача Xrdp до групи ssl-cert..."
-sudo adduser xrdp ssl-cert
-sudo adduser root xrdp
+#echo "Додаємо користувача Xrdp до групи ssl-cert..."
+#sudo adduser xrdp ssl-cert
+#sudo adduser root xrdp
 
 # Перезапускаємо Xrdp
 echo "Перезапуск Xrdp..."
@@ -36,6 +36,11 @@ echo "gnome-session" > ~/.xsession
 # Перезапускаємо Xrdp ще раз для застосування змін
 echo "Перезапуск Xrdp для застосування налаштувань GNOME..."
 sudo systemctl restart xrdp
+
+echo "Створення звичайного користувача через якого будемо підключатись"
+echo "*потрібно для коректної роботи певних програм"
+read -p "Введіть ім'я нового користувача: " new_user
+sudo adduser $new_user
 
 # Виводимо повідомлення про завершення
 echo "Скрипт завершено. Тепер ви можете підключатися до вашого сервера через RDP."
